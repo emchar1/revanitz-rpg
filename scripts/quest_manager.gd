b0e4ea584a2bc15200f95920f6f05ba51509ff9d
JakeTDA <naingminkhant15126@gmail.com>
2026-07-16 18:12:25 -0400

    more updates

--- scripts/quest_manager.gd
+++ scripts/quest_manager.gd
@@ -11,16 +11,22 @@ var quest : Quest:
 			
 func mob_slain(enemy):
 	if quest != null:
+		print("eyy")
 		if quest.objective == "Kill" and quest.object == enemy.entity_name:
 			quest.num -= 1
+			if quest.num <= 0:
+				quest.status = "Complete"
+				print(quest.status)
+				%Label.text = ""
+			print(quest.num)
 	#print("mob quest")
 
 func item_get(obj):
 	if quest != null:
 		if quest.objective == "Fetch" and quest.object == obj.obj_name:
 			quest.num -= 1
-		if quest.num <= 0:
-			quest.status = "Complete"
-			print(quest.status)
-			%Label.text = ""
-		print(quest.num)
+			if quest.num <= 0:
+				quest.status = "Complete"
+				print(quest.status)
+				%Label.text = ""
+			print(quest.num)
